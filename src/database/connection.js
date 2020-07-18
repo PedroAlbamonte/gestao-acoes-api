4f3d7711b9eda31268f175bfa520470fa940c64c
Pedro Albamonte <pedro.albamonte@livelo.com.br>
2020-07-18 12:55:25 -0300

    fix enviroment

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
-const configuration = require('../../knexfile');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../knexfile')[environment];
 
-const connection = knex(configuration.development);
+const connection = knex(configuration);
 
 module.exports = connection;
