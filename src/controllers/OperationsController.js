f1f6a2ca9c3bb60da955d7fc504b560008ab0b07
Pedro Albamonte <pedro.albamonte@livelo.com.br>
2020-07-18 17:41:29 -0300

    fix authorization

--- src/controllers/OperationsController.js
+++ src/controllers/OperationsController.js
@@ -2,6 +2,7 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(request, response) {
+    console.log(request.user);
     const { page = 1 } = request.query;
 
     const [count] = await connection('operacao').count();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const GoogleAuth = require('simple-google-openid');
+
 const port = process.env.PORT || 3333;
+const environment = process.env.NODE_ENV || 'development';
 const app = express();
+
+var googleClientId = process.env.GOOGLE_CLIENT_ID || 'client_id';
+console.log(googleClientId);
+app.use(GoogleAuth(googleClientId));
+app.use(GoogleAuth.guardMiddleware());
 
 app.use(cors());
 app.use(express.json());
 app.use(function(req, res, next) {
-    if(req.headers['x-forwarded-proto'] !== 'https') {
+    if( environment !== 'development' && req.headers['x-forwarded-proto'] !== 'https') {
         return res.status(403).send({message: 'SSL required'});
     }
     // allow the request to continue
